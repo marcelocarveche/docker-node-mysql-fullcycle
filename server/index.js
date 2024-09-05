4ab57917a9e4344f5e5c577efc7d52f462a2840d
marcelocarveche <marcelocarveche@gmail.com>
2024-09-05 13:47:28 -0400

    add mysql db

--- server/index.js
+++ server/index.js
@@ -5,21 +5,25 @@ const app = express();
 const port = 3000;
 
 
-// const config = {
-//     host: 'db',
-//     user: 'root',
-//     password: 'root',
-//     database: 'nodedb'
-// }
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
 
-// try {
-//     const connection = await mysql.createConnection(config)
-//     connection.query(`CREATE TABLE people (id int not null auto_increment, name varchar(255), primary key(id))`);
-//     connection.query(`INSERT INTO people(name) values('Carveche')`);
-//     connection.end();
-// } catch (error) {
-//     console.log("Erro ao conectar com o banco de dados: ", error);
-// }
+try {
+    const connection = mysql.createConnection(config)
+    // connection.query(`CREATE TABLE people (id int not null auto_increment, name varchar(255), primary key(id))`);
+    // connection.query(`INSERT INTO people(name) values('Carveche')`);
+    connection.query(`SELECT * FROM people`, (err, result, fields) => {
+        if (err) throw err;
+        console.log(result);
+    })
+    connection.end();
+} catch (error) {
+    console.log("Erro ao conectar com o banco de dados: ", error);
+}
 
 app.get('/', (req, res) => {
     res.send('Desenvolvimento com Docker & NodeJS');
